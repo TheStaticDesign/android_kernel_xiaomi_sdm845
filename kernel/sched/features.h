a8dd83254c76ee13e6bfeb522d6f52954495d74c
Quentin Perret <quentin.perret@arm.com>
2018-10-10 16:15:51 +0100

    EXPERIMENTAL: ANDROID: sched/fair: Make EAS use the simplified Energy Model
    EAS now supports two flavours of Energy Model:
     - the legacy, per sched-domain one;
     - the upstream, simplified one.
    
    Introduce a sched_feat enabling to switch between the two at runtime.
    When sched_feat(EAS_SIMPLIFIED_EM) is on,  EAS uses
    compute_energy_simplified() in the wake-up path, hence reducing the
    calculation time.
    
    The new default is to use the simplified EM.
    
    Change-Id: Ic692afcb15d070b58a6ff981b930b6582a3d3234
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Udit Karode <udit.karode@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -102,3 +102,9 @@ SCHED_FEAT(FBT_STRICT_ORDER, false)
  * OFF: schedtune.prefer_idle is honored as is.
  */
 SCHED_FEAT(EAS_USE_NEED_IDLE, false)
+
+/*
+ * Use the Simplified Energy Model for EAS accounting only for
+ * active costs of CPUs.
+ */
+SCHED_FEAT(EAS_SIMPLIFIED_EM, true)

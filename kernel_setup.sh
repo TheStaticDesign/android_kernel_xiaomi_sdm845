8ec60b5d0202860f3ac5b97c8bdcaf45d3b2cfd0
starlight5234 <starlight5234@protonmail.ch>
2020-09-28 10:07:53 +0000

    .circleci: Init CircleCI
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

---
+++ kernel_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 CloudedQuartz
+#
+
+# Script to set up environment to build an android kernel
+# Assumes required packages are already installed
+
+# Config
+CURRENT_DIR="$(pwd)"
+KERNELNAME="Common"
+KERNEL_DIR="$CURRENT_DIR"
+AK_REPO="https://github.com/xiaomi-sdm845-devel/AnyKernel3"
+AK_DIR="$HOME/AnyKernel3"
+TC_DIR="$HOME/proton-clang"
+# End Config
+
+# clone_tc - clones proton clang to TC_DIR
+clone_tc() {
+	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git $TC_DIR
+}
+
+# Clones anykernel
+clone_ak() {
+	git clone $AK_REPO $AK_DIR
+}
+# Actually do stuff
+clone_tc
+clone_ak
+
+# Run build script
+. ${CURRENT_DIR}/kernel_build.sh

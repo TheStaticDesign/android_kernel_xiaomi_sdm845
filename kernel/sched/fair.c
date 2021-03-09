b93f43ae3f44f62d426ce0d83ef24f5b88cf7c74
CloudedQuartz <ravenklawasd@gmail.com>
2021-03-09 20:00:22 +0530

    sched: fair: mark scale_rt_capacity as maybe unused
    This is unused for now as deadline utilization tracking
    is used, but this may be useful in the future.
    
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9449,7 +9449,7 @@ static inline int get_sd_load_idx(struct sched_domain *sd,
 	return load_idx;
 }
 
-static unsigned long scale_rt_capacity(int cpu)
+static unsigned long __maybe_unused scale_rt_capacity(int cpu)
 {
 	struct rq *rq = cpu_rq(cpu);
 	unsigned long max = arch_scale_cpu_capacity(NULL, cpu);

3e711df3e7c92530bdc190133c70e3102b156df3
CloudedQuartz <ravenklawasd@gmail.com>
2021-04-29 18:34:32 +0530

    sched/fair: remove util_est remains
    - We do not have pelt or util_est
    
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7762,7 +7762,7 @@ static void nohz_balancer_kick(bool only_update);
 static unsigned long cpu_util_next(int cpu, struct task_struct *p, int dst_cpu)
 {
 	struct cfs_rq *cfs_rq = &cpu_rq(cpu)->cfs;
-	unsigned long util_est, util = READ_ONCE(cfs_rq->avg.util_avg);
+	unsigned long util = READ_ONCE(cfs_rq->avg.util_avg);
 
 	/*
 	 * If @p migrates from @cpu to another, remove its contribution. Or,
@@ -7774,21 +7774,6 @@ static unsigned long cpu_util_next(int cpu, struct task_struct *p, int dst_cpu)
 		util = max_t(long, util - task_util(p), 0);
 	else if (task_cpu(p) != cpu && dst_cpu == cpu)
 		util += task_util(p);
-
-	if (sched_feat(UTIL_EST)) {
-		util_est = READ_ONCE(cfs_rq->avg.util_est.enqueued);
-
-		/*
-		 * During wake-up, the task isn't enqueued yet and doesn't
-		 * appear in the cfs_rq->avg.util_est.enqueued of any rq,
-		 * so just add it (if needed) to "simulate" what will be
-		 * cpu_util() after the task has been enqueued.
-		 */
-		if (dst_cpu == cpu)
-			util_est += _task_util_est(p);
-
-		util = max(util, util_est);
-	}
 
 	return min_t(unsigned long, util, capacity_orig_of(cpu));
 }
